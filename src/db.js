1b3a74be097bcc661b7f7595aced0d53da4742c8
Pedro Lucas <pedrolucassousa416@gmail.com>
2025-11-15 12:24:18 -0300

    Configurando sequelize para usar DATABASE_URL do Render

--- src/db.js
+++ src/db.js
@@ -1,16 +1,30 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+const { Sequelize } = require('sequelize');
+const connectionUrl = process.env.DATABASE_URL;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+let sequelize;
+
+if (connectionUrl) {
+  sequelize = new Sequelize(connectionUrl, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false 
+      }
+    },
+    logging: false 
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME || 'api_unibalsas',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASS || '', 
+    {
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'mysql'
+    }
+  );
+}
 
 module.exports = sequelize;
